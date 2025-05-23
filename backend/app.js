5b6beb215f7afdea3db6ff74dc3ba3f984c7ae9b
Debottam Kar <debottamkar2003@gmail.com>
2025-05-23 10:47:27 +0530

    ui ux

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,26 @@ import express from 'express';
 import cors from 'cors';
 import pool from './dbconnect.js';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 const app = express();
 app.use(cors());
+const __filename = fileURLToPath(import.meta.url);
+// console.log(import.meta);
+console.log(__filename);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+
+});
 
 app.get('/api/home', (req, res) => {
     res.json({
@@ -46,14 +59,13 @@ app.post('/api/reviewer', (req, res) => {
     });
 });
 
-
 async function dbconnection() {
     try {
         const client = await pool.connect();
         console.log('Connected to the database');
         app.listen(process.env.PORT, () => {
-            console.log('Server is running on port 5000');
-            console.log('http://localhost:5000');
+            console.log(`Server is running on port ${process.env.PORT}`);
+            console.log(`http://localhost: ${process.env.PORT}`);
         });
         client.release();
         let {rows} = await pool.query('select * from movies limit 1');
