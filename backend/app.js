5d11c8fb21a62cc71885c37a3ba755b4da0cac40
Debottam Kar <debottamkar2003@gmail.com>
2025-05-23 16:13:21 +0530

    Author Registertration api, Reviewer Registration api, Author login api, Reviewer Login api done

--- backend/app.js
+++ backend/app.js
@@ -15,45 +15,146 @@ const __dirname = path.dirname(__filename);
 console.log(__dirname);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
-
 });
 
-app.get('/api/home', (req, res) => {
-    res.json({
-        message: 'Welcome to the home page!'
-    });
+app.post('/api/authorRegister', express.json(), async (req, res) => {
+    const { email, password, firstname, lastname } = req.body;
+    if (!email || !password || !firstname || !lastname) {
+        return res.status(400).json({
+            message: 'Please provide email and password'
+        });
+    }
+    console.log(email, password);
+    try {
+        let checkEmail = await pool.query('select email from authors where email = $1', [email]);
+        if (checkEmail.rows.length > 0) {
+            return res.status(400).json({
+                message: 'Email already exists'
+            });
+        }
+        let data = await pool.query('insert into authors(email, password, firstname, lastname) values($1, $2, $3, $4) returning email', [email, password, firstname, lastname]);
+        console.log(data.rows);
+        res.json({
+            message: 'Welcome to the paperHunt!',
+            email: data.rows[0].email
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Error in Author Registration'
+        });
+    }
 });
 
-app.post("/api/login", (req, res) => {
-    res.json({
-        message: 'Welcome to the login page!'
-    });
+app.post("/api/reviewerRegister", express.json(), async (req, res) => {
+    let { email, password, firstname, lastname, phonenumber, affiliation } = req.body;
+    if (!email || !password || !firstname || !lastname || !phonenumber || !affiliation) {
+        return res.status(400).json({
+            message: 'Please provide all the fields'
+        });
+    }
+    try {
+        let checkEmail = await pool.query('select email from reviewers where email = $1', [email]);
+        if (checkEmail.rows.length > 0) {
+            return res.status(400).json({
+                message: 'Email already exists'
+            });
+        }
+        let data = await pool.query('insert into reviewers(email, password, firstname, lastname, phonenumber, affiliation) values($1, $2, $3, $4, $5, $6) returning email', [email, password, firstname, lastname, phonenumber, affiliation]);
+        console.log(data.rows);
+        res.json({
+            message: 'Welcome to the paperHunt!',
+            email: data.rows[0].email
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Error in Reviewer Registration'
+        });
+    }
 });
 
-app.post("/api/register", (req, res) => {
-    res.json({
-        message: 'Welcome to the register page!'
-    });
+app.post("/api/authorLogin", express.json(), async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({
+            message: 'Please provide email and password'
+        });
+    }
+    console.log(email, password);
+    try {
+        let data = await pool.query("select email, password from authors where email = $1", [email]);
+        if (data.rows.length === 0) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+        else if (data.rows[0].password !== password) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+        else {
+            res.json({
+                message: 'Welcome back to the paperHunt again!',
+                email: data.rows[0].email
+            });
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Error in Author Login'
+        });
+    }
 });
 
-app.get('/api/report', (req, res) => {
-    res.json({
-        message: 'Welcome to the report page!'
-    });
+app.post('/api/reviewerLogin', express.json(), async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({
+            message: 'Please provide email and password'
+        });
+    }
+    console.log(email, password);
+    try {
+        let data = await pool.query("select email, password from reviewers where email = $1", [email]);
+        console.log(data.rows);
+        if (data.rows.length === 0) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+        else if (data.rows[0].password !== password) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+        else {
+            res.json({
+                message: 'Welcome back to the paperHunt again!',
+                email: data.rows[0].email
+            });
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Error in Reviewer Login'
+        });
+    }
 });
 
-app.post('/api/author', (req, res) => {
+app.post('/api/authorPaperSubmit', (req, res) => {
     res.json({
         message: 'Welcome to the author page!'
     });
 });
 
-app.post('/api/reviewer', (req, res) => {
+app.post('/api/reviewerReviewSubmit', (req, res) => {
     res.json({
         message: 'Welcome to the reviewer page!'
     });
@@ -68,7 +169,7 @@ async function dbconnection() {
             console.log(`http://localhost: ${process.env.PORT}`);
         });
         client.release();
-        let {rows} = await pool.query('select * from movies limit 1');
+        let { rows } = await pool.query('select * from movies limit 1');
         console.log(rows);
     } catch (err) {
         console.error('Error connecting to the database', err);
