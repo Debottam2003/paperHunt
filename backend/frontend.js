782490ff289287ec4ab88a7ba78b7795734bc3b4
Debottam Kar <debottamkar2003@gmail.com>
2025-06-16 02:29:28 +0530

    fixing routing

--- backend/frontend.js
+++ backend/frontend.js
@@ -16,6 +16,38 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+app.get('/choose', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'enter.html'));
+});
+
+app.get('/authorLogin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'authorLogin.html'));
+});
+
+app.get('/authorRegister', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'authorRegister.html'));
+});
+
+app.get('/authorDashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'authorDashboard.html'));
+});
+
+app.get('/reviewerLogin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'reviewerLogin.html'));
+});
+
+app.get('/reviewerRegister', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'reviewerRegister.html'));
+});
+
+app.get('/reviewerDashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'reviewerDashboard.html'));
+});
+
+app.get('/papers', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'papers.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
